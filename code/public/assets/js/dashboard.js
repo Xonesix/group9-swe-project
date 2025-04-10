2d37ee14ff321f5b0bb254ba46c8a40d5afcc319
Hudson K. Green <HudsonGTV@users.noreply.github.com>
2025-04-10 14:40:09 -0500

    commented some of the javascript in dashboard.js to make more readable

--- code/public/assets/js/dashboard.js
+++ code/public/assets/js/dashboard.js
@@ -14,22 +14,36 @@ window.onload = async () => {
 const formDisplay = document.querySelector(".add-team");
 const createButton = document.querySelector(".create-team");
 const cancelButton = document.querySelector(".cancel-button");
+
+// Show form when create team button is clicked
 createButton.addEventListener("click", (event) => {
   formDisplay.classList.add("active");
 });
+
+// Hide form when cancel button is clicked
 cancelButton.addEventListener("click", (e) => {
   formDisplay.classList.remove("active");
 });
 
 // Team form submission
 const form = document.getElementById("create-team-form");
+
+// Add event listener to form submission (when user clicks submit on create team dialog)
 form.addEventListener("submit", async (event) => {
+
   event.preventDefault();
+
   try {
+    
+    // Get the form data from the form element
     const formData = new FormData(form);
 
+    // Convert formData into an Object
     const data = Object.fromEntries(formData.entries());
+
     console.log(data);
+
+    // Send the form data to the server using fetch (and wait for a server response)
     const response = await fetch("/api/protected/create-team", {
       method: "POST",
       headers: {
@@ -37,16 +51,22 @@ form.addEventListener("submit", async (event) => {
       },
       body: JSON.stringify(data),
     });
+
+    // Check if the response is ok (status code 200-299)
     if (response.ok) {
+      // Replace the team card holder with a new one to refresh the teams
       document.querySelector(".team-card-holder").replaceChildren();
       await hydrateTeams();
+      // Hide the form popup
       formDisplay.classList.remove("active");
     } else {
       throw Error("Somethign went wrong with server");
     }
+
   } catch (error) {
     console.error(`Something went wrong when submitting form: ${error}`);
   }
+
 });
 
 // // Hydrate Dashboard
@@ -65,18 +85,27 @@ form.addEventListener("submit", async (event) => {
 //     account_name.textContent = result.email;
 //   } catch (error) {}
 // }
-// Hydrate TEams
+// Hydrate Teams
+// Pretty sure this function just fetches the teams from the server and populates the team cards on the dashboard -Hudson
 async function hydrateTeams() {
+
   const template = document.getElementById("team-card-template");
 
   try {
+
+    // Fetch teams from the server
     const response = await fetch("/api/protected/get-teams", {
       method: "GET",
     });
+    // Check if the response is ok (status code 200-299) (await until response is received)
     const result = await response.json();
+
+    // Check if the response was successful (if not, throw an error)
     if (!result.success) {
       throw Error("Couldn't Fetch Teams");
     }
+
+    // Populate the wrapper with the team cards
     const teams = result.teams;
     for (const team of teams) {
       const clone = document.importNode(template.content, true);
@@ -84,7 +113,9 @@ async function hydrateTeams() {
       clone.querySelector(".chat-button").href = `/chat?uuid=${team.id}`;
       document.querySelector(".team-card-holder").appendChild(clone);
     }
+    
   } catch (error) {
     console.error(`Error: ${error}`);
   }
+
 }
